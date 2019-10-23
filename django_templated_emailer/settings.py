70b852a7a90139bb256e220782f65a5ac33c0a88
iarp <ian@iarp.ca>
2019-10-23 14:55:00 -0400

    Cleaned up settings a bit, added ability to accept params for field types

--- django_templated_emailer/settings.py
+++ django_templated_emailer/settings.py
@@ -2,8 +2,13 @@ from django.conf import settings
 from django.utils.module_loading import import_string
 from django.db import models
 
+
+def _setting(name, default=None):
+    return getattr(settings, f'TEMPLATED_EMAILER_{name}', default)
+
+
 # Whether or not to log the result of the sender task.
-CELERY_IGNORE_RESULT = getattr(settings, 'TEMPLATED_EMAILER_CELERY_TASK_SENDER_IGNORE_RESULT', False)
+CELERY_IGNORE_RESULT = _setting('CELERY_TASK_SENDER_IGNORE_RESULT', False)
 
 tmp = settings.SETTINGS_MODULE.split('.')
 if len(tmp) == 1:
@@ -11,22 +16,29 @@ if len(tmp) == 1:
 else:
     project = '.'.join(tmp[:-1])
 
-CELERY_APP_IMPORT_PATH = getattr(settings, 'TEMPLATED_EMAILER_CELERY_APP', f'{project}.celery.app')
+CELERY_APP_IMPORT_PATH = _setting('CELERY_APP', f'{project}.celery.app')
 try:
     PROJECT_CELERY_APP = import_string(CELERY_APP_IMPORT_PATH)
 except ImportError:
     PROJECT_CELERY_APP = None
 
 # Default model field type for Template and Queue
-BODY_FIELD_TYPE = getattr(settings, 'TEMPLATED_EMAILER_DEFUALT_BODY_FIELD_TYPE', models.TextField())
+BODY_FIELD_TYPE = _setting('DEFAULT_BODY_FIELD_TYPE', models.TextField)
 
 # Allow overriding Template and Queue body field types.
-TEMPLATE_BODY_FIELD_TYPE = getattr(settings, 'TEMPLATED_EMAILER_QUEUE_BODY_FIELD_TYPE', BODY_FIELD_TYPE)
-QUEUE_BODY_FIELD_TYPE = getattr(settings, 'TEMPLATED_EMAILER_TEMPLATE_BODY_FIELD_TYPE', BODY_FIELD_TYPE)
+TEMPLATE_BODY_FIELD_TYPE = _setting('TEMPLATE_BODY_FIELD_TYPE', BODY_FIELD_TYPE)
+TEMPLATE_BODY_FIELD_PARAMS = _setting('TEMPLATE_BODY_FIELD_PARAMS', {})
+if isinstance(TEMPLATE_BODY_FIELD_TYPE, str):
+    TEMPLATE_BODY_FIELD_TYPE = import_string(TEMPLATE_BODY_FIELD_TYPE)
+
+QUEUE_BODY_FIELD_TYPE = _setting('QUEUE_BODY_FIELD_TYPE', BODY_FIELD_TYPE)
+QUEUE_BODY_FIELD_PARAMS = _setting('QUEUE_BODY_FIELD_PARAMS', {})
+if isinstance(QUEUE_BODY_FIELD_TYPE, str):
+    QUEUE_BODY_FIELD_TYPE = import_string(QUEUE_BODY_FIELD_TYPE)
 
 # Allow changing template name field where default=True?
 # Setting this to true can cause EmailQueue.queue_email(template_name='...') because someone changed the name.
-TEMPLATE_DEFAULT_ALLOW_CHANGING_NAME = getattr(settings, 'TEMPLATED_EMAILER_ALLOW_DEFAULT_CHANGE_NAME', False)
+TEMPLATE_DEFAULT_ALLOW_CHANGING_NAME = _setting('ALLOW_DEFAULT_CHANGE_NAME', False)
 
 # Do you want to allow deletion of default templates?
-TEMPLATE_DEFAULT_ALLOW_DELETE = getattr(settings, 'TEMPLATED_EMAILER_ALLOW_DEFAULT_DELETE', False)
+TEMPLATE_DEFAULT_ALLOW_DELETE = _setting('ALLOW_DEFAULT_DELETE', False)
