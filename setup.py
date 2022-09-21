fa74876ab450e3dbae65d573165456f5d2f9d441
iarp <iarp.opensource@gmail.com>
2022-09-21 10:36:28 -0400

    change(core): change setup.py to use find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-templated-emailer',
@@ -8,7 +8,7 @@ setup(
     author='IARP',
     author_email='iarp.opensource@gmail.com',
     license='MIT',
-    packages=['django_templated_emailer'],
+    packages=find_packages(),
     install_requires=["django>=1.11", "requests"],
     zip_safe=False
 )
